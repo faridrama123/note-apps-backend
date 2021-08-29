b7a349b74f075167f4d6693d21426a525fd9b0a5
faridrama123 <systemcalls915@gmail.com>
2021-08-29 17:07:52 +0700

    authorization implemented

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -4,9 +4,8 @@ const routes = require('./routes');
 module.exports = {
   name: 'notes',
   version: '1.0.0',
-  register: async (server, { service , validator}) => {
+  register: async (server, { service, validator }) => {
     const notesHandler = new NotesHandler(service, validator);
     server.route(routes(notesHandler));
-
   },
 };
